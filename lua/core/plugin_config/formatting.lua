85f6b89d8a9f6e15b3dc43c7fabc7835eb233cd8
Romain Mrad <romain.mrad@gmail.com>
2024-05-26 20:08:02 +0200

    Adding new plugins

---
+++ lua/core/plugin_config/formatting.lua
@@ -0,0 +1,24 @@
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "isort", "black" },
+		java = { "astyle" },
+		c = { "astyle", "clang-format" },
+		cpp = { "astyle", "clang-format" },
+		json = { "prettier" },
+		markdown = { "prettier" },
+	},
+	format_on_save = {
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	},
+})
+
+vim.keymap.set({ "n", "v" }, "<leader>l", function()
+	require("conform").format({
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	})
+end)
