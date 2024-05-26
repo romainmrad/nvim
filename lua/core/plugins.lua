47545f2f51ee3fc62f0b907b17194cba3d3b94f2
Romain Mrad <romain.mrad@gmail.com>
2024-05-26 20:07:42 +0200

    Adding new plugins

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,9 +1,9 @@
 local ensure_packer = function()
 	local fn = vim.fn
-	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-		vim.cmd [[packadd packer.nvim]]
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
 		return true
 	end
 	return false
@@ -11,35 +11,53 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'nvim-tree/nvim-tree.lua'
-	use 'nvim-tree/nvim-web-devicons'
-	use 'nvim-lualine/lualine.nvim'
-	use 'nvim-treesitter/nvim-treesitter'
-	use {
-	  'nvim-telescope/telescope.nvim',
-	  branch = '0.1.x',
-	  requires = { { 'nvim-lua/plenary.nvim' } } 
-	}
-	use {
-		'terrortylor/nvim-comment',
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+	use("nvim-tree/nvim-tree.lua")
+	use("nvim-tree/nvim-web-devicons")
+	use("nvim-lualine/lualine.nvim")
+	use("nvim-treesitter/nvim-treesitter")
+	use({
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({
+		"terrortylor/nvim-comment",
 		config = function()
 			require("nvim_comment").setup({ create_mappings = false })
-		end
-	}
-	use {
-		'akinsho/bufferline.nvim', 
-		version = "*", 
-		dependencies = 'nvim-tree/nvim-web-devicons' 
-	}
-	use 'lewis6991/gitsigns.nvim'
-	use {
-		"akinsho/toggleterm.nvim", tag = '*', config = function()
+		end,
+	})
+	use({
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+	})
+	use("lewis6991/gitsigns.nvim")
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+		config = function()
 			require("toggleterm").setup()
-		end
-	}
+		end,
+	})
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
+	use("stevearc/conform.nvim")
+	use("mfussenegger/nvim-lint")
 	if packer_bootstrap then
-		require('packer').sync()
+		require("packer").sync()
 	end
 end)
