449e76ab781e02b805ee09800fad132c3d2fa6fc
Romain Mrad <romain.mrad@gmail.com>
2024-05-28 21:35:30 +0200

    Add: tree ignores git folder and relative number is show next to files

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,6 +1,10 @@
 vim.o.loaded_netrw = 1
 vim.o.loaded_netrwPlugin = 1
 
-require('nvim-tree').setup()
+require("nvim-tree").setup({
+	filters = {
+		custom = { "^\\.git$", ".idea", ".vscode" },
+	},
+})
 
-vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<cr>")
+vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<cr> :setlocal relativenumber<cr>")
